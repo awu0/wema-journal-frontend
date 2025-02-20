a5e50b09c2c19b4b1a28c9d0b7dc90956c895748
Eric Dong <ed2303@nyu.edu>
2025-02-20 11:32:07 -0500

    some stuff to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes,} from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useParams,
+} from 'react-router-dom';
 
 import './App.css';
 
 import Navbar from './Components/Navbar';
 import People from './Components/People';
 import {PersonPage} from "./Components/People/PersonPage";
+
+function PersonPage() {
+  const { name } = useParams();
+  return <h1>{name}</h1>
+}
+
+//function Home() {
+//  const styles = {
+//    'text-align': 'center',
+//  }
+//  return <h1 style={styles} >Journal of WEMA</h1>
+//}
 
 function App() {
   return (
