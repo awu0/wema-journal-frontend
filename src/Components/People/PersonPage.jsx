b51f843ccb4b160120b659c7bb0599ccb0c09d7f
Aaron Wu <82164146+awu0@users.noreply.github.com>
2025-02-20 21:23:47 -0500

    fixes on PersonPage.jsx; roles as list isn't being fetched

--- src/Components/People/PersonPage.jsx
+++ src/Components/People/PersonPage.jsx
@@ -5,10 +5,6 @@ import { BACKEND_URL } from "../../constants";
 
 const PEOPLE_READ_ENDPOINT = `${BACKEND_URL}/users`;
 
-const personObjectToArray = (data) => {
-  return Array.isArray(data) ? data : [data];
-};
-
 export function PersonPage() {
   const [person, setPerson] = useState(null);
   const [error, setError] = useState(null);
@@ -18,7 +14,8 @@ export function PersonPage() {
     axios
       .get(`${PEOPLE_READ_ENDPOINT}/${email}`)
       .then(({ data }) => {
-        setPerson(personObjectToArray(data));
+        console.log(data);
+        setPerson(data);
       })
       .catch(() => {
         setError(`There was a problem getting ${email}. They might not exist.`);
@@ -32,19 +29,22 @@ export function PersonPage() {
   if (!person && !error) {
     return <p>Loading...</p>;
   }
-
+  
   return (
     <div>
       <h1>Person Information</h1>
-      <p>Email: {email}</p>
       {/* Handling error */}
       {error ? <p>{error}</p> : null}
       {/* Display person information if no error */}
+
       {person && (
-        <>
-          <p>Name: {person.name}</p>
-          <p>More information: ...</p>
-        </>
+          <>
+            <p>Name: {person.name}</p>
+            <p>Email: {person.email}</p>
+            {/* Roles aren't getting the list */}
+            <p>Role: {person.roles}</p>
+            <p>Affiliation: {person.affiliation}</p>
+          </>
       )}
     </div>
   );
