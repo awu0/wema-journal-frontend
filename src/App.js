2ff81f078bcd187169e3a29b8f518412a8873b00
Eric Dong <ed2303@nyu.edu>
2025-02-20 20:34:59 -0500

    fixing errors that broken frontend

--- src/App.js
+++ src/App.js
@@ -3,35 +3,27 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  useParams,
 } from 'react-router-dom';
 
 import './App.css';
 
 import Navbar from './Components/Navbar';
 import People from './Components/People';
-import {PersonPage} from "./Components/People/PersonPage";
-
-function PersonPage() {
-  const { name } = useParams();
-  return <h1>{name}</h1>
-}
-
-//function Home() {
-//  const styles = {
-//    'text-align': 'center',
-//  }
-//  return <h1 style={styles} >Journal of WEMA</h1>
-//}
+import { PersonPage } from "./Components/People/PersonPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        {/* For a different home page, do:
-         <Route index element={<Login />} /> */}
+        {/* Home page setup */}
+        {/* For a different home page, you can add an index route here */}
+        {/* <Route index element={<Home />} /> */}
+
+        {/* People list route */}
         <Route path="people" element={<People />} />
+        
+        {/* Dynamic route for individual person */}
         <Route path="people/:email" element={<PersonPage />} />
       </Routes>
     </BrowserRouter>
--- src/Components/People/PersonPage.jsx
+++ src/Components/People/PersonPage.jsx
@@ -1,28 +1,51 @@
-import {useParams} from "react-router-dom";
-import React from "react";
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {BACKEND_URL} from "../../constants";
+import { BACKEND_URL } from "../../constants";
 
 const PEOPLE_READ_ENDPOINT = `${BACKEND_URL}/users`;
 
 const personObjectToArray = (data) => {
-
-}
+  return Array.isArray(data) ? data : [data];
+};
 
 export function PersonPage() {
-    const [person, setPerson] = React.useState([]);
+  const [person, setPerson] = useState(null);
+  const [error, setError] = useState(null);
+  const { email } = useParams();
 
-    const {email} = useParams();
+  const fetchPerson = () => {
+    axios
+      .get(`${PEOPLE_READ_ENDPOINT}/${email}`)
+      .then(({ data }) => {
+        setPerson(personObjectToArray(data));
+      })
+      .catch(() => {
+        setError(`There was a problem getting ${email}. They might not exist.`);
+      });
+  };
 
-    const fetchPerson = () => {
-        axios.get(PEOPLE_READ_ENDPOINT)
-            .then(
-                ({data}) => {
-                    setPerson(personObjectToArray(data))
-                }
-            )
-            .catch((error) => setError(`There was a problem getting ${email}. They might not exist.`));
-    };
+  useEffect(() => {
+    fetchPerson();
+  }, [email]);
 
-    return <h1>{email}</h1>
+  if (!person && !error) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h1>Person Information</h1>
+      <p>Email: {email}</p>
+      {/* Handling error */}
+      {error ? <p>{error}</p> : null}
+      {/* Display person information if no error */}
+      {person && (
+        <>
+          <p>Name: {person.name}</p>
+          <p>More information: ...</p>
+        </>
+      )}
+    </div>
+  );
 }
