9fdf08210c885ab8cb5ce08c5d45d29f9b5a34f7
W1LL9999 <william@osiris.cyber.nyu.edu>
2025-02-22 00:15:50 -0500

    added boilerplate for submissions

---
+++ src/Components/Submissions/SubmissionsPage.jsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../../constants";
+
+
+export function SubmissionPage() {
+
+}
