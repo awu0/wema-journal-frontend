85ccfdac27ca0c90a99dca632fd7ba11c72cde62
Aaron Wu <82164146+awu0@users.noreply.github.com>
2025-03-04 22:47:21 -0500

    displays all submissions

--- src/Components/Submissions/Submissions.jsx
+++ src/Components/Submissions/Submissions.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {BACKEND_URL} from "../../constants";
+import axios from "axios";
+
+const SUBMISSIONS_READ_ENDPOINT = `${BACKEND_URL}/manuscripts`;
+
+const Manuscript = (manuscript) => {
+    const {title, author, content, publication_date, state} = manuscript
+    
+    return <div>
+        <h1>Title: {title}</h1>
+        <p>Author: {author}</p>
+        <p>Content: {content}</p>
+        <p>Date published: {publication_date}</p>
+        <p>State: {state}</p>
+    </div>
+}
 
 function Submissions() {
+    const [manuscripts, setManuscripts] = useState([]);
+    const [error, setError] = useState('');
+    
+    // fetch manuscript Data
+    const fetchManuscripts = () => {
+        axios.get(SUBMISSIONS_READ_ENDPOINT)
+          .then(
+            ({data}) => {
+                setManuscripts(data);
+            }
+          )
+          .catch((error) => setError(`There was a problem retrieving the list of people. ${error}`));
+    };
+
+    useEffect(fetchManuscripts, []);
+    
     return (
         <div>
+            {manuscripts.map((manuscript) => Manuscript(manuscript))}
+            { error }
         </div>
     );
 }
