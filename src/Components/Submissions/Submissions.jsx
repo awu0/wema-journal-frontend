732e616a7127c9f09341974be98e4b35e923972f
Aaron Wu <82164146+awu0@users.noreply.github.com>
2025-03-04 22:51:27 -0500

    added basic styling

--- src/Components/Submissions/Submissions.jsx
+++ src/Components/Submissions/Submissions.jsx
@@ -1,25 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import {BACKEND_URL} from "../../constants";
 import axios from "axios";
+import {Link} from "react-router-dom";
 
 const SUBMISSIONS_READ_ENDPOINT = `${BACKEND_URL}/manuscripts`;
 
 const Manuscript = (manuscript) => {
     const {title, author, content, publication_date, state} = manuscript
     
-    return <div>
-        <h1>Title: {title}</h1>
-        <p>Author: {author}</p>
-        <p>Content: {content}</p>
-        <p>Date published: {publication_date}</p>
-        <p>State: {state}</p>
+    return <div className="person-container">
+        <Link to={`${SUBMISSIONS_READ_ENDPOINT}/${title}`}>
+            <h2>Title: {title}</h2>
+            <p>Author: {author}</p>
+            <p>Content: {content}</p>
+            <p>Date published: {publication_date}</p>
+            <p>State: {state}</p>
+        </Link>
     </div>
 }
 
 function Submissions() {
     const [manuscripts, setManuscripts] = useState([]);
     const [error, setError] = useState('');
-    
+
     // fetch manuscript Data
     const fetchManuscripts = () => {
         axios.get(SUBMISSIONS_READ_ENDPOINT)
