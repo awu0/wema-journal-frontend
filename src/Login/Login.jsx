fbea258317404ce9718ef78a66d4f41d606a4862
Eric Dong <ed2303@nyu.edu>
2025-03-14 15:14:22 -0400

    starting the css for login page, work in progress

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../App.css';
+import './Login.css';
 
 function Login() {
   const [username, setUsername] = useState('');
