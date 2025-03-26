bbd9a903616ed0ac78d8374fabaa34b6a7d5cfba
Aaron Wu <82164146+awu0@users.noreply.github.com>
2025-03-25 23:51:39 -0400

    fixed formatting for manuscript table

--- src/Components/Manuscripts/Manuscripts.jsx
+++ src/Components/Manuscripts/Manuscripts.jsx
@@ -7,68 +7,59 @@ import './Manuscripts.css';
 const MANUSCRIPTS_READ_ENDPOINT = `${BACKEND_URL}/manuscripts`;
 
 const ManuscriptRow = (manuscript) => {
-    const {title, author, content, publication_date, state} = manuscript
-    
-    
-    return (
-      <tr>
-          <td>
-              <Link to={`manuscripts/${title}`}>
-                  <td>{title}</td>
-                  <td>{author}</td>
-                  <td>{content}</td>
-                  <td>{publication_date}</td>
-                  <td>{state}</td>
-              </Link>
-          </td>
-      </tr>
-    )
+  const {title, author, content, publication_date, state} = manuscript
 
-    // return <div className="manuscript-container">
-    //     <Link to={`manuscripts/${title}`}>
-    //         <h2>Title: {title}</h2>
-    //         <p>Author: {author}</p>
-    //         <p>Content: {content}</p>
-    //         <p>Date published: {publication_date}</p>
-    //         <p>State: {state}</p>
-    //     </Link>
-    // </div>
+  return (
+    <tr>
+      <td>
+        <Link to={`manuscripts/${title}`}>
+          {title}
+        </Link>
+      </td>
+      <td>{author}</td>
+      <td>{content}</td>
+      <td>{publication_date}</td>
+      <td>{state}</td>
+    </tr>
+  )
 }
 
 function ViewManuscripts() {
-    const [manuscripts, setManuscripts] = useState([]);
-    const [error, setError] = useState('');
+  const [manuscripts, setManuscripts] = useState([]);
+  const [error, setError] = useState('');
 
-    // fetch manuscript Data
-    const fetchManuscripts = () => {
-        axios.get(MANUSCRIPTS_READ_ENDPOINT)
-          .then(
-            ({data}) => {
-                setManuscripts(data);
-            }
-          )
-          .catch((error) => setError(`There was a problem retrieving the list of people. ${error}`));
-    };
+  // fetch manuscript Data
+  const fetchManuscripts = () => {
+    axios.get(MANUSCRIPTS_READ_ENDPOINT)
+      .then(
+        ({data}) => {
+          setManuscripts(data);
+        }
+      )
+      .catch((error) => setError(`There was a problem retrieving the list of people. ${error}`));
+  };
 
-    useEffect(fetchManuscripts, []);
-    
-    return (
-        <div className={"wrapper"}>
-            <table className={'people-table'}>
-                <thead>
-                <tr>
-                    <td>Title</td>
-                    <td>Author</td>
-                    <td>Content</td>
-                    <td>Date published</td>
-                    <td>State</td>
-                </tr>
-                </thead>
-            </table>
-            {manuscripts.map((manuscript) => ManuscriptRow(manuscript))}
-            { error }
-        </div>
-    );
+  useEffect(fetchManuscripts, []);
+
+  return (
+    <div className={"wrapper"}>
+      <table className={'people-table'}>
+        <thead>
+        <tr>
+          <td>Title</td>
+          <td>Author</td>
+          <td>Content</td>
+          <td>Date published</td>
+          <td>State</td>
+        </tr>
+        </thead>
+        <tbody>
+        {manuscripts.map((manuscript) => ManuscriptRow(manuscript))}
+        </tbody>
+      </table>
+      {error}
+    </div>
+  );
 }
 
 
