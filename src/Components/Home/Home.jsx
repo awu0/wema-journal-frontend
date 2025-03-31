00cf395a9f74a28c75451dcf82e002a6ec81cc47
Eric Dong <ed2303@nyu.edu>
2025-03-31 14:58:13 -0400

    changed home page a bit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,24 +10,34 @@ function Home() {
     };
 
     const handleViewSubmissions = () => {
+        navigate('/manuscripts');
+    };
+
+    const handleViewMission = () => {
+        navigate('/about');
+    };
+
+    const handleViewSubmit = () => {
         navigate('/submissions');
     };
 
     return (
         <div className="home-container">
-            <h1>Our Mission</h1>
-            <p></p>
-            
-            <h2>Submit Manuscript</h2>
-            <p></p>
-            
+            <h1>WEMA Journal</h1>
+            <p>Read about our mission statement</p>
+            <button className="view-mission-btn" onClick={handleViewMission}>View Our Mission</button>
+
             <h2>Meet Our Team</h2>
             <p>Get to know the people who make this platform possible.</p>
             <button className="view-people-btn" onClick={handleViewPeople}>View People</button>
+            
+            <h2>Submit Manuscript</h2>
+            <p>Submit a manuscript and have it reviewed and published</p>
+            <button className="view-people-btn" onClick={handleViewSubmit}>Go Submit a Manuscript</button>
 
             <h2>View Submissions</h2>
             <p>Check the list of submitted manuscripts.</p>
-            <button className="view-submissions-btn" onClick={handleViewSubmissions}>View Submissions</button>
+            <button className="view-submissions-btn" onClick={handleViewSubmissions}>View Submitted Manuscripts</button>
         </div>
     );
 }
