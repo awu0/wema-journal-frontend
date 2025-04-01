cf6cc6dbb2d480def573565bf1a0b0f78f653ebe
Aaron Wu <82164146+awu0@users.noreply.github.com>
2025-04-01 15:31:57 -0400

    can now sort the manuscript table by headers

--- src/Components/Manuscripts/Manuscripts.jsx
+++ src/Components/Manuscripts/Manuscripts.jsx
@@ -28,6 +28,34 @@ function ViewManuscripts() {
   const [manuscripts, setManuscripts] = useState([]);
   const [error, setError] = useState('');
 
+  const [sortConfig, setSortConfig] = useState({key: "title", direction: "asc"});
+
+  const handleSort = (key) => {
+    let direction = "asc";
+
+    // toggle sorting order if the same column is clicked again
+    if (sortConfig.key === key && sortConfig.direction === "asc") {
+      direction = "desc";
+    }
+
+    setSortConfig({key, direction});
+  };
+
+  const sortedManuscripts = [...manuscripts].sort((a, b) => {
+    let manuscriptA = a[sortConfig.key];
+    let manuscriptB = b[sortConfig.key];
+
+    if (typeof manuscriptA === "string") {
+      manuscriptA = manuscriptA.toLowerCase();
+      manuscriptB = manuscriptB.toLowerCase();
+    }
+
+    if (manuscriptA < manuscriptB) return sortConfig.direction === "asc" ? -1 : 1;
+    if (manuscriptA > manuscriptB) return sortConfig.direction === "asc" ? 1 : -1;
+
+    return 0;
+  })
+
   // fetch manuscript Data
   const fetchManuscripts = () => {
     axios.get(MANUSCRIPTS_READ_ENDPOINT)
@@ -46,15 +74,20 @@ function ViewManuscripts() {
       <table className={'people-table'}>
         <thead>
         <tr>
-          <td>Title</td>
-          <td>Author</td>
-          <td>Content</td>
-          <td>Date published</td>
-          <td>State</td>
+          <td
+            onClick={() => handleSort("title")}>Title {sortConfig.key === "title" ? (sortConfig.direction === "asc" ? "▲" : "▼") : ""}</td>
+          <td
+            onClick={() => handleSort("author")}>Author {sortConfig.key === "author" ? (sortConfig.direction === "asc" ? "▲" : "▼") : ""}</td>
+          <td
+            onClick={() => handleSort("content")}>Content {sortConfig.key === "content" ? (sortConfig.direction === "asc" ? "▲" : "▼") : ""}</td>
+          <td
+            onClick={() => handleSort("datePublished")}>Date published {sortConfig.key === "datePublished" ? (sortConfig.direction === "asc" ? "▲" : "▼") : ""}</td>
+          <td
+            onClick={() => handleSort("state")}>State{sortConfig.key === "state" ? (sortConfig.direction === "asc" ? "▲" : "▼") : ""}</td>
         </tr>
         </thead>
         <tbody>
-        {manuscripts.map((manuscript) => ManuscriptRow(manuscript))}
+        {sortedManuscripts.map((manuscript) => ManuscriptRow(manuscript))}
         </tbody>
       </table>
       {error}
