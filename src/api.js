a125119ad16e8367e556fe762fe62041e2797f2e
W1LL9999 <william@osiris.cyber.nyu.edu>
2025-04-17 01:07:50 -0400

    moving api calls

--- src/api.js
+++ src/api.js
@@ -11,3 +11,17 @@ export const login = async (email, password) => {
   return axios.post(`${BACKEND_URL}/auth/login`, { email, password });
 };
 
+export const getManuscripts = async () => {
+  return axios.get(`${BACKEND_URL}/manuscripts`);
+};
+
+// Extracted from Masthead component
+export const getMasthead = async () => {
+  return axios.get(`${BACKEND_URL}/users/masthead`);
+};
+
+// People object to array transform helper
+export const peopleObjectToArray = (data) => {
+  const keys = Object.keys(data);
+  return keys.map((key) => data[key]);
+};
