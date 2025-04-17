e04f17d9140526355bfe6229571741808e456c5f
W1LL9999 <william@osiris.cyber.nyu.edu>
2025-04-17 01:10:43 -0400

    added more api calls

--- src/api.js
+++ src/api.js
@@ -7,6 +7,22 @@ export const getUsers = async () => {
   return axios.get(`${BACKEND_URL}/users`);
 };
 
+export const getUserByEmail = async (email) => {
+  return axios.get(`${BACKEND_URL}/users/${email}`);
+};
+
+export const updateUser = async (email, userData) => {
+  return axios.patch(`${BACKEND_URL}/users/${email}`, userData);
+};
+
+export const deleteUser = async (email) => {
+  return axios.delete(`${BACKEND_URL}/users/${email}`);
+};
+
+export const getRoles = async () => {
+  return axios.get(`${BACKEND_URL}/roles`);
+};
+
 export const login = async (email, password) => {
   return axios.post(`${BACKEND_URL}/auth/login`, { email, password });
 };
