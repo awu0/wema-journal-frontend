762d37bf0d35e02557d36c6e50909507956e6fb1
Aaron Wu <82164146+awu0@users.noreply.github.com>
2025-05-08 16:58:44 -0400

    deleted console.log

--- src/Components/Manuscripts/ManuscriptPage.jsx
+++ src/Components/Manuscripts/ManuscriptPage.jsx
@@ -1,34 +1,34 @@
 import React, {useEffect, useState} from "react";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import * as api from "../../api";
-import { useUser } from '../../userContext';
-import { MANUSCRIPT_STATES } from '../../constants';
+import {useUser} from '../../userContext';
+import {MANUSCRIPT_STATES} from '../../constants';
 
 
 export function ManuscriptPage() {
   const {_id} = useParams();
-  
+
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [abstract, setAbstract] = useState("");
   const [content, setContent] = useState("");
   const [submission_date, setSubmission_date] = useState("");
   const [referees, setReferees] = useState([])
-  
+
   const [originalState, setOriginalState] = useState("");
   const [state, setState] = useState('');
   const validStates = Object.entries(MANUSCRIPT_STATES);
-  const { user } = useUser();
+  const {user} = useUser();
   const isEditor = user?.roles?.includes('editor');
 
   const [refereeForm, setRefereeForm] = useState('');
 
   const handleUpdateState = () => {
-    api.manuscriptReceiveAction(_id, originalState,  state, refereeForm)
+    api.manuscriptReceiveAction(_id, originalState, state, refereeForm)
       .then(() => fetchManuscript())
       .catch((err) => console.error("Failed to update state:", err));
   };
-  
+
   const fetchManuscript = () => {
     api.getManuscriptsById(_id)
       .then(({data}) => {
@@ -46,7 +46,6 @@ export function ManuscriptPage() {
   useEffect(() => {
     fetchManuscript()
   }, [_id]);
-  console.log(referees)
 
   return (
     <div>
@@ -61,7 +60,7 @@ export function ManuscriptPage() {
         {referees.map((ref, index) => (
           <li key={index}>{ref}</li>
         ))}</p>
-      
+
       {isEditor ? (
         <div>
           <label htmlFor="state">Update State:</label>
@@ -76,10 +75,10 @@ export function ManuscriptPage() {
           </select>
 
           {state === MANUSCRIPT_STATES.ASSIGN_REF && (
-           <>
-             <label htmlFor="referee">Referee:</label>
-             <input value={refereeForm} onChange={(e) => setRefereeForm(e.target.value.trim())}/>
-           </>  
+            <>
+              <label htmlFor="referee">Referee:</label>
+              <input value={refereeForm} onChange={(e) => setRefereeForm(e.target.value.trim())}/>
+            </>
           )}
 
           {state === MANUSCRIPT_STATES.DELETE_REF && (
@@ -98,7 +97,7 @@ export function ManuscriptPage() {
               </select>
             </>
           )}
-          
+
           <button onClick={handleUpdateState}>Update State</button>
         </div>
       ) : (
